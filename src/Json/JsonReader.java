444b60982bb713e27a6686e8e6dce18d0d166123
heiyami <heiyamiii@gmail.com>
2015-12-27 04:51:58 -0800

    Wrote stubs.

---
+++ src/Json/JsonReader.java
@@ -0,0 +1,20 @@
+package json;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Created by Julian on 2015-12-24.
+ */
+public class JsonReader {
+
+    public static JSONArray readJsonArrayFromUrl(String url) {
+        //TODO: Complete the implementation
+        return new JSONArray(); //stub
+    }
+
+    public static JSONObject readJsonObjectFromUrl(String url) {
+        //TODO: Complete the implementation
+        return new JSONObject(); //stub
+    }
+}
