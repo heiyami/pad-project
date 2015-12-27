5d29306e59d4ad203b0fb30c3d24f5aeb687dde3
heiyami <heiyamiii@gmail.com>
2015-12-26 20:18:01 -0800

    Added Awakening class. Added fields for number of certain Awakenings. Introduced idea of "playstyle" to help search (e.g. combo, rainbow).

--- src/model/Team.java
+++ src/model/Team.java
@@ -1,23 +1,43 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Julian on 2015-12-23.
  */
 public class Team {
 
+    private List<String> playstyles; //Playstyle should be something like: Combo, Rainbow, Wood, TPA...
     private ArrayList<Monster> monsters;
+    private List<Awakening> totalAwakenings;
     private int totalCost;
+    private int totalAtkFire;
+    private int totalAtkWater;
+    private int totalAtkWood;
+    private int totalAtkLight;
+    private int totalAtkDark;
+    private int totalHp;
+    private int totalRcv;
+    private int totalDarkResist;
+    private int totalJammerResist;
+    private int totalPoisonResist;
+    private int totalSkillBindResist;
+
 
     public Team() {
         monsters = new ArrayList<Monster>();
         totalCost = 0;
+        totalDarkResist = 0;
+        totalJammerResist = 0;
+        totalPoisonResist = 0;
+        totalSkillBindResist = 0;
     }
 
     public void addMonster(Monster m) {
         monsters.add(m);
         totalCost += m.getCost();
+        totalAwakenings.addAll(m.getAwakenings());
     }
 
     public void removeMonster(int i) {
@@ -38,4 +58,6 @@ public class Team {
     public int getTotalCost() {
         return totalCost;
     }
+
+
 }
